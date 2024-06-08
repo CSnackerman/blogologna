cbce27f7dee48cbd137a6017577950d27fc8b404
ca <colbyackerman@gmail.com>
2024-06-08 12:26:52 -0500

    misc

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, test } from 'vitest';
 import { toLowerKebab } from '~/utils/converters';
-import { fakeFetch } from '~/utils/data';
 
 describe.concurrent('Suite: utils', () => {
   test('Test: toLowerKebab()', async () => {
@@ -9,13 +8,5 @@ describe.concurrent('Suite: utils', () => {
     const result = toLowerKebab(subject);
 
     expect(result).toEqual('hello-how-are-we-doing');
-  });
-
-  test('Test: fakeFetch()', async () => {
-    const data = { hello: 'there' };
-
-    await expect(fakeFetch(data, 50, 0)).resolves.toEqual(data);
-
-    await expect(fakeFetch(data, 50, 100)).rejects.toThrowError('fake fetch failed');
   });
 });
