b87788c0d23f2350c2a5a5adf2f874968896f2da
ca <colbyackerman@gmail.com>
2024-06-09 15:14:37 -0500

    disable prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// prettier-ignore
 export default defineNuxtConfig({
-  modules: ['@nuxt/test-utils/module', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/test-utils/module', 
+    '@nuxt/eslint', 
+    '@nuxtjs/supabase',
+  ],
   devtools: { enabled: false },
   css: ['~/assets/styles/base.scss'],
   imports: {
