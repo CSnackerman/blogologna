83f6b236c4dc1bc89377ec7001a52be069f07123
ca <colbyackerman@gmail.com>
2024-06-10 19:03:27 -0500

    add page content

--- pages/whoami.vue
+++ pages/whoami.vue
@@ -1,7 +1,84 @@
 <template>
-  <h2>Who am I?</h2>
+  <div class="whoami-page">
+    <h2>Who am I?</h2>
+    <p>Hey there :) my name is Colby—sometimes I go by Col.</p>
+    <p>
+      I like playing and making games <br />
+      <span class="shrink">(mostly playing haha)</span>
+    </p>
+    <p>I live in St. Louis, Missouri.</p>
+    <p>
+      Lately I have just been working on a pair of projects as portfolio pieces <br />
+      to get hired again somewhere as a software engineer.
+      <span class="shrink">(this is one of them)</span>
+    </p>
+    <div class="check-it-out">
+      <label>Check it out!</label>
+      <a href="https://resume-fishing.web.app/">Gone Fishing</a>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .check-it-out {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > label {
+      position: relative;
+      font-weight: bold;
+
+      &::after {
+        content: '*work in progress';
+        padding-left: 2px;
+        color: green;
+        position: absolute;
+        width: max-content;
+        top: 0;
+        left: 100%;
+        font-size: 0.55em;
+        font-weight: normal;
+      }
+    }
+
+    & > a {
+      all: unset;
+      padding: 1em;
+      color: blue;
+      transition: all 150ms ease-in;
+
+      &:hover {
+        color: cyan;
+        font-size: 1.2em;
+        font-weight: bold;
+        text-shadow: 1px 1px 2px black;
+        text-decoration: underline;
+        text-underline-offset: 7px;
+        text-decoration-thickness: 2px;
+        cursor: pointer;
+
+        &::after {
+          content: '🎣';
+        }
+        &::before {
+          content: '🐟';
+        }
+      }
+    }
+  }
+
+  .note {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    font-size: 0.7em;
+  }
+
+  .shrink {
+    font-size: 0.7em;
+  }
+</style>
